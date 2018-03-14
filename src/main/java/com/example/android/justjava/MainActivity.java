c51ed68f4ba6d3a9184cfbc0e7eddd526eb79187
Kenneth Wong <kennethwty93@gmail.com>
2018-03-14 18:01:53 -0400

    Added app src

---
+++ src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,73 @@
+/**
+ * IMPORTANT: Make sure you are using the correct package name.
+ * This example uses the package name:
+ * package com.example.android.justjava
+ * If you get an error when copying this code into Android studio, update it to match teh package name found
+ * in the project's AndroidManifest.xml file.
+ **/
+
+package com.example.android.justjava;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+    int quantity = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        //int numberOfCoffee = quantity;
+        //display(numberOfCoffee);
+        String priceMessage = "Total: $" + (quantity * 5) + "\nThank You!";
+        displayPrice(priceMessage);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method calculates the price based on the number of cups of coffee selected
+     */
+    private void displayPrice(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
+
+    /**
+     * This method increments the number of coffee
+     */
+    public void increment(View view) {
+        ++quantity;
+        display(quantity);
+    }
+
+    /**
+     * This is the decrement method for the minus sign
+     */
+    public void decrement(View view) {
+        if (quantity > 0) {
+            --quantity;
+            display(quantity);
+        } else {
+            quantity = 0;
+        }
+    }
+}
